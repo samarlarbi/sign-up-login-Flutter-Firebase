223acb94f000e1ce6c3b9b2f39ec8c706ed5f1ae
samar <samarlarbi2004@gmail.com>
2024-06-11 12:32:07 +0100

    adding firebase

--- lib/SignUpPage.dart
+++ lib/SignUpPage.dart
@@ -26,65 +26,69 @@ class SignUpPage extends StatelessWidget {
               },
             ),
           ),
-          body: Container(
-            padding: EdgeInsets.all(30),
-            child: Column(
-              children: [
-                Text(
-                  "Sign Up.",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w700,
-                    color: ColorPalete().color1,
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                children: [
+                  Text(
+                    "Sign Up.",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w700,
+                      color: ColorPalete().color1,
+                    ),
                   ),
-                ),
-                Column(
-                  children: [
-                    CInput(
-                        name: "new account name",
-                        hintText: "name",
-                        obscureText: false),
-                    CInput(
-                        name: "Gmail", hintText: "Gmail", obscureText: false),
-                    CInput(
-                        name: "password", hintText: "name", obscureText: true),
-                    CInput(
-                        name: "confirm Password",
-                        hintText: "name",
-                        obscureText: true),
-                  ],
-                ),
-                CustomButton(
-                    widget: LandingPage(),
-                    title: "sign Up",
-                    bgcolor: ColorPalete().color1,
-                    titlecolor: Colors.white),
-                Padding(    
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  Column(
                     children: [
-                      Text(
-                        "Already have an account ? ",
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 162, 161, 161),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Text(
-                          'login ',
-                          style: TextStyle(
-                            color: ColorPalete().color1,
-                            decoration: TextDecoration.underline,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
+                      CInput(
+                          name: "new account name",
+                          hintText: "name",
+                          obscureText: false),
+                      CInput(
+                          name: "Gmail", hintText: "Gmail", obscureText: false),
+                      CInput(
+                          name: "password",
+                          hintText: "name",
+                          obscureText: true),
+                      CInput(
+                          name: "confirm Password",
+                          hintText: "name",
+                          obscureText: true),
                     ],
                   ),
-                )
-              ],
+                  CustomButton(
+                      widget: LandingPage(),
+                      title: "sign Up",
+                      bgcolor: ColorPalete().color1,
+                      titlecolor: Colors.white),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Already have an account ? ",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 162, 161, 161),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Text(
+                            'login ',
+                            style: TextStyle(
+                              color: ColorPalete().color1,
+                              decoration: TextDecoration.underline,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           )),
     );
